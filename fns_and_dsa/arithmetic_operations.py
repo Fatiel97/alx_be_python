b3fb1c159eea4b37bdeed4bb84cfff246545c015
fati <f.elhoudaigui97@gmail.com>
2024-07-07 14:12:47 -0700

    Initial commit

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1, num2, operation):
+    """Perform basic arithmetic operations."""
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Cannot divide by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
